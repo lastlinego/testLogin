c0967bd4c6be2005a6eb167fbfc516bd63ee4b49
Sasha <Sasha@Sashas-Mac-mini.local>
2018-03-26 10:27:20 +0300

    added new xpath

--- src/test/java/HomeWork1.java
+++ src/test/java/HomeWork1.java
@@ -8,13 +8,17 @@ public class HomeWork1 {
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void login(){
+    public void login() throws InterruptedException {
         driver.get("https://rozetka.com.ua/");
-        driver.findElement(By.xpath("//*[@id=\"header_user_menu_parent\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"popup_signin\"]/div[1]/div[1]/input")).sendKeys("vova@yopmail.com");
-        driver.findElement(By.xpath("//*[@id=\"popup_signin\"]/div[1]/div[2]/div[1]/div[1]/input")).sendKeys("Gfhjkm123");
-        driver.findElement(By.xpath("//*[@id=\"popup_signin\"]/div[1]/div[2]/div[1]/div[2]/div/span/button")).click();
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//*[@id='header_user_menu_parent']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@name='login']")).sendKeys("vova@yopmail.com");
+        driver.findElement(By.xpath("(//input[@name='password'])[1]")).sendKeys("Gfhjkm123");
+        driver.findElement(By.xpath("//*[@id='popup_signin']/div[1]/div[2]/div[1]/div[2]/div/span/button")).click();
         driver.quit();
 
     }
+
+
 }
