391eb6bbf8069c1227cc6a63ad937c00f3577a1d
Sasha <Sasha@Sashas-Mac-mini.local>
2018-03-28 17:55:07 +0300

    check the cart

---
+++ src/test/java/CheckTheCart.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CheckTheCart extends BeforeAfter {
+    @Test
+    public void checkTheCart() throws InterruptedException {
+        driver.findElement(By.xpath("//span[@class='xhr'][contains(text(), 'Корзина')]")).click();
+    }
+
+}
