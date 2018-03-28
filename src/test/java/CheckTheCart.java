d64a42690dd6d85053873ec61d0ebdf51e15670f
Alex <alex@Pros-MacBook-Pro.local>
2018-03-28 22:01:57 +0300

    check the cart done

--- src/test/java/CheckTheCart.java
+++ src/test/java/CheckTheCart.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckTheCart extends BeforeAfter {
     @Test
     public void checkTheCart() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='xhr'][contains(text(), 'Корзина')]")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//h2[@class='empty-cart-title inline sprite-side']")).getText().contains("Корзина пуста"));
+
     }
 
 }
